3ba1f768d87d4d17a40538432e5f711f67197425
Shatilla Prayer <shatilla.prayer@unlv.edu>
2016-11-18 12:47:27 -0800

    Added full page review with mobile view

--- src/app/print/print.component.ts
+++ src/app/print/print.component.ts
@@ -20,4 +20,44 @@ export class PrintComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  imageSrc(pic){
+    return "assets/images/" + pic;
+  }
+
+  goTo(index){
+    this.data.value = index;
+    if(this.data.value == this.assignments.length-1){
+      this.printContent();
+    }else{
+      this.hidePrintContent();
+    }
+  }
+
+  printContent(){
+    this.data.print = true;
+    // If any of the buttons have not been selected, trigger error message
+    // If they user selects all the assignments, then the print area becomes visible
+    // loop goes through 1-7
+    // It only checks types that have two answers
+    for(let x=1; x<=this.data.assignments.length-2; x++){
+      // check if category type is the two choices
+      if(this.data.assignments[x].type=="two"){
+        // then check if both selectA and selectB is false
+        // this ensures they actually select something
+        if(this.data.assignments[x].taskA.selectA == false && this.data.assignments[x].taskB.selectB==false){
+          this.data.error.open = true;
+          this.data.value = 2;
+          this.hidePrintContent();
+        }else{
+          this.data.error.open = false;
+        }
+      }
+    }
+  }
+
+  // hide the print area
+  hidePrintContent(){
+    this.data.print = false;
+  }
 }
